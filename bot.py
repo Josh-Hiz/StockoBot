8fef18a90e0b76c51f28cf974297987e49b0c643
Josh H <jhizgiaev2704@gmail.com>
2023-01-13 02:44:50 -0500

    Bot Commit

---
+++ bot.py
@@ -0,0 +1,126 @@
+#import os
+import os
+
+#discord import 
+import discord
+from discord.ext import commands
+
+#import dotenv 
+from dotenv import load_dotenv
+
+#import data science tools
+from pandas_datareader import data as pdr, get_data_enigma
+import pandas as pd
+import numpy as np
+import requests
+import yfinance as yf
+from scipy import stats
+import matplotlib.pyplot as plt
+import mplfinance as mpf
+import disnake
+
+#Time tooling
+from datetime import datetime, timedelta
+from newsapi.newsapi_client import NewsApiClient
+from requests import *
+import json
+
+months = ['January','Febuary','March',
+          'April','May','June','July',
+          'August', 'September', 'October',
+          'November','December']
+
+yf.pdr_override()
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+NEWS_TOKEN = os.getenv('NEWS_API_TOKEN')
+
+clientIntents = discord.Intents.all()
+
+helpCommand = commands.DefaultHelpCommand(no_category='Avaliable Commands')
+
+client = commands.Bot(command_prefix='!stocko ', intents=clientIntents,help_command=helpCommand)
+
+@client.event
+async def on_ready():
+    await client.wait_until_ready()
+    for guild in client.guilds:
+            if guild.name == GUILD:
+                break    
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+def get_data(stock, start_date, end_date):
+    df = pdr.get_data_yahoo(stock, start = start_date, end = end_date)
+    return df
+
+@client.command(name='Graph-Performance',help="Returns stock performance over a specified interval of months")
+async def chart(ctx,
+                chart_type = commands.parameter(default='Line',description="Chart type: Line, Candle, Renko, OHLC, Point-Figure"), 
+                year1 = commands.parameter(description="1970-Present"), 
+                month1= commands.parameter(description="1-12"), 
+                day1 = commands.parameter(description="Depends on the month, but usually 1-30"), 
+                year2 = commands.parameter(description="1970-Presnet"), 
+                month2 = commands.parameter(description="1-12"), 
+                day2 = commands.parameter(description="Depends on the month, but usually 1-30"), 
+                stock = commands.parameter(description="Ticker symbol of stock, ex: AAPL, GOOGL...")):
+    '''Purely done for UI purposes, I really did not want to use this but there is no other way'''
+    
+    right_time_point = datetime(int(year2), int(month2), int(day2))
+    left_time_point = datetime(int(year1), int(month1), int(day1))
+    
+    if(left_time_point > right_time_point or right_time_point > datetime.today() or left_time_point > datetime.today()):
+        await ctx.send("ERROR: Cannot have the first time point greater than the second time point OR any time thats greater than today")
+        raise discord.DiscordException("ERROR: Cannot have the first time point greater than the second time point")
+    else:
+        await ctx.send(f'Years selected: {year1}-{year2}, stock selected: {stock}, first time point: {left_time_point}, second time point: {right_time_point}')
+        
+        data = get_data(stock, left_time_point.strftime('%Y-%m-%d'), right_time_point.strftime('%Y-%m-%d'))
+        
+        plt.clf()
+        
+        colors = mpf.make_marketcolors(up="#00ff00",
+                                       down="#ff0000",
+                                       wick="inherit",
+                                       edge="inherit",
+                                       volume="orange",
+                                       ohlc='orange')
+        
+        mpf_style = mpf.make_mpf_style(base_mpf_style='nightclouds',marketcolors = colors)
+        
+        match chart_type:
+            case 'Line':
+                mpf.plot(data,type='line',style=mpf_style,title=f'Stock Price between {months[int(month1)-1]} {day1} and {months[int(month2)-1]} {day2} within {year1}-{year2}',savefig='output.png')
+            case 'Candle':
+                mpf.plot(data, type='candle',style=mpf_style,title=f'Stock Price between {months[int(month1)-1]} {day1} {year1} and {months[int(month2)-1]} {day2} {year2}',savefig='output.png')
+            case 'Renko':
+                mpf.plot(data,type='renko',style=mpf_style,title=f'Stock Price between {months[int(month1)-1]} {day1} and {months[int(month2)]-1} {day2} within {year1}-{year2}',savefig='output.png',renko_params=dict(brick_size=0.75))
+            case 'Point-Figure':
+                mpf.plot(data,type='pnf',style=mpf_style, title=f'Stock Price between {months[int(month1)-1]} {day1} and {months[int(month2)-1]} {day2} within {year1}-{year2}',savefig='output.png')
+            case 'OHLC':
+                mpf.plot(data,type='ohlc',style=mpf_style, title=f'Stock Price between {months[int(month1)-1]} {day1} and {months[int(month2)-1]} {day2} within {year1}-{year2}',savefig='output.png')
+            
+        filename = 'output.png'
+        await ctx.send(file=discord.File(filename))
+
+def parse_json(stockJson):
+    '''Should only have Close, Open, Volume, Low, High'''
+    pass
+  
+@client.command(name='RealTime', help='Shows realtime statistics of a specified stock')
+async def stock_realtime(ctx, symbol:str):
+    url = f'https://query1.finance.yahoo.com/v8/finance/chart/{symbol}?interval=1m&range=1d&close=adjusted'
+    
+    response = requests.get(url,headers={'User-agent': 'Mozilla/5.0'})
+    d = response.json()
+    data = parse_json(d) 
+        
+        
+client.run(TOKEN)
---
+++ lib/python3.10/site-packages/disnake/types/audit_log.py
@@ -0,0 +1,331 @@
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+import datetime
+from typing import List, Literal, Optional, TypedDict, Union
+
+from typing_extensions import NotRequired
+
+from .automod import (
+    AutoModAction,
+    AutoModEventType,
+    AutoModRule,
+    AutoModTriggerMetadata,
+    AutoModTriggerType,
+)
+from .channel import ChannelType, PermissionOverwrite, VideoQualityMode
+from .guild import (
+    DefaultMessageNotificationLevel,
+    ExplicitContentFilterLevel,
+    MFALevel,
+    VerificationLevel,
+)
+from .guild_scheduled_event import GuildScheduledEvent
+from .integration import IntegrationExpireBehavior, PartialIntegration
+from .interactions import ApplicationCommand, ApplicationCommandPermissions
+from .role import Role
+from .snowflake import Snowflake
+from .threads import Thread
+from .user import User
+from .webhook import Webhook
+
+AuditLogEvent = Literal[
+    1,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    30,
+    31,
+    32,
+    40,
+    41,
+    42,
+    50,
+    51,
+    52,
+    60,
+    61,
+    62,
+    72,
+    73,
+    74,
+    75,
+    80,
+    81,
+    82,
+    83,
+    84,
+    85,
+    90,
+    91,
+    92,
+    100,
+    101,
+    102,
+    110,
+    111,
+    112,
+    121,
+    140,
+    141,
+    142,
+    143,
+]
+
+
+class _AuditLogChange_Str(TypedDict):
+    key: Literal[
+        "name",
+        "description",
+        "preferred_locale",
+        "vanity_url_code",
+        "topic",
+        "code",
+        "allow",
+        "deny",
+        "permissions",
+        "tags",
+    ]
+    new_value: str
+    old_value: str
+
+
+class _AuditLogChange_AssetHash(TypedDict):
+    key: Literal[
+        "icon_hash",
+        "splash_hash",
+        "discovery_splash_hash",
+        "banner_hash",
+        "avatar_hash",
+        "asset",
+    ]
+    new_value: str
+    old_value: str
+
+
+class _AuditLogChange_Snowflake(TypedDict):
+    key: Literal[
+        "id",
+        "owner_id",
+        "afk_channel_id",
+        "rules_channel_id",
+        "public_updates_channel_id",
+        "widget_channel_id",
+        "system_channel_id",
+        "application_id",
+        "channel_id",
+        "inviter_id",
+        "guild_id",
+    ]
+    new_value: Snowflake
+    old_value: Snowflake
+
+
+class _AuditLogChange_Bool(TypedDict):
+    key: Literal[
+        "widget_enabled",
+        "nsfw",
+        "hoist",
+        "mentionable",
+        "temporary",
+        "deaf",
+        "mute",
+        "nick",
+        "enabled_emoticons",
+        "region",
+        "rtc_region",
+        "available",
+        "archived",
+        "locked",
+        "premium_progress_bar_enabled",
+        "enabled",
+    ]
+    new_value: bool
+    old_value: bool
+
+
+class _AuditLogChange_Int(TypedDict):
+    key: Literal[
+        "afk_timeout",
+        "prune_delete_days",
+        "position",
+        "bitrate",
+        "rate_limit_per_user",
+        "color",
+        "max_uses",
+        "max_age",
+        "user_limit",
+        "auto_archive_duration",
+        "default_auto_archive_duration",
+    ]
+    new_value: int
+    old_value: int
+
+
+class _AuditLogChange_ListSnowflake(TypedDict):
+    key: Literal["exempt_roles", "exempt_channels"]
+    new_value: List[Snowflake]
+    old_value: List[Snowflake]
+
+
+class _AuditLogChange_ListRole(TypedDict):
+    key: Literal["$add", "$remove"]
+    new_value: List[Role]
+    old_value: List[Role]
+
+
+class _AuditLogChange_MFALevel(TypedDict):
+    key: Literal["mfa_level"]
+    new_value: MFALevel
+    old_value: MFALevel
+
+
+class _AuditLogChange_VerificationLevel(TypedDict):
+    key: Literal["verification_level"]
+    new_value: VerificationLevel
+    old_value: VerificationLevel
+
+
+class _AuditLogChange_ExplicitContentFilter(TypedDict):
+    key: Literal["explicit_content_filter"]
+    new_value: ExplicitContentFilterLevel
+    old_value: ExplicitContentFilterLevel
+
+
+class _AuditLogChange_DefaultMessageNotificationLevel(TypedDict):
+    key: Literal["default_message_notifications"]
+    new_value: DefaultMessageNotificationLevel
+    old_value: DefaultMessageNotificationLevel
+
+
+class _AuditLogChange_ChannelType(TypedDict):
+    key: Literal["type"]
+    new_value: ChannelType
+    old_value: ChannelType
+
+
+class _AuditLogChange_IntegrationExpireBehaviour(TypedDict):
+    key: Literal["expire_behavior"]
+    new_value: IntegrationExpireBehavior
+    old_value: IntegrationExpireBehavior
+
+
+class _AuditLogChange_VideoQualityMode(TypedDict):
+    key: Literal["video_quality_mode"]
+    new_value: VideoQualityMode
+    old_value: VideoQualityMode
+
+
+class _AuditLogChange_Overwrites(TypedDict):
+    key: Literal["permission_overwrites"]
+    new_value: List[PermissionOverwrite]
+    old_value: List[PermissionOverwrite]
+
+
+class _AuditLogChange_Datetime(TypedDict):
+    key: Literal["communication_disabled_until"]
+    new_value: datetime.datetime
+    old_value: datetime.datetime
+
+
+class _AuditLogChange_ApplicationCommandPermissions(TypedDict):
+    key: str
+    new_value: ApplicationCommandPermissions
+    old_value: ApplicationCommandPermissions
+
+
+class _AuditLogChange_AutoModTriggerType(TypedDict):
+    key: Literal["trigger_type"]
+    new_value: AutoModTriggerType
+    old_value: AutoModTriggerType
+
+
+class _AuditLogChange_AutoModEventType(TypedDict):
+    key: Literal["event_type"]
+    new_value: AutoModEventType
+    old_value: AutoModEventType
+
+
+class _AuditLogChange_AutoModActions(TypedDict):
+    key: Literal["actions"]
+    new_value: List[AutoModAction]
+    old_value: List[AutoModAction]
+
+
+class _AuditLogChange_AutoModTriggerMetadata(TypedDict):
+    key: Literal["trigger_metadata"]
+    new_value: AutoModTriggerMetadata
+    old_value: AutoModTriggerMetadata
+
+
+AuditLogChange = Union[
+    _AuditLogChange_Str,
+    _AuditLogChange_AssetHash,
+    _AuditLogChange_Snowflake,
+    _AuditLogChange_Int,
+    _AuditLogChange_Bool,
+    _AuditLogChange_ListSnowflake,
+    _AuditLogChange_ListRole,
+    _AuditLogChange_MFALevel,
+    _AuditLogChange_VerificationLevel,
+    _AuditLogChange_ExplicitContentFilter,
+    _AuditLogChange_DefaultMessageNotificationLevel,
+    _AuditLogChange_ChannelType,
+    _AuditLogChange_IntegrationExpireBehaviour,
+    _AuditLogChange_VideoQualityMode,
+    _AuditLogChange_Overwrites,
+    _AuditLogChange_Datetime,
+    _AuditLogChange_ApplicationCommandPermissions,
+    _AuditLogChange_AutoModTriggerType,
+    _AuditLogChange_AutoModEventType,
+    _AuditLogChange_AutoModActions,
+    _AuditLogChange_AutoModTriggerMetadata,
+]
+
+
+class AuditEntryInfo(TypedDict):
+    delete_member_days: str
+    members_removed: str
+    channel_id: Snowflake
+    message_id: Snowflake
+    count: str
+    id: Snowflake
+    type: Literal["0", "1"]
+    role_name: str
+    application_id: Snowflake
+    auto_moderation_rule_name: str
+    auto_moderation_rule_trigger_type: str
+
+
+class AuditLogEntry(TypedDict):
+    target_id: Optional[str]
+    changes: NotRequired[List[AuditLogChange]]
+    user_id: Optional[Snowflake]
+    id: Snowflake
+    action_type: AuditLogEvent
+    options: NotRequired[AuditEntryInfo]
+    reason: NotRequired[str]
+
+
+class AuditLog(TypedDict):
+    audit_log_entries: List[AuditLogEntry]
+    application_commands: List[ApplicationCommand]
+    auto_moderation_rules: List[AutoModRule]
+    guild_scheduled_events: List[GuildScheduledEvent]
+    integrations: List[PartialIntegration]
+    threads: List[Thread]
+    users: List[User]
+    webhooks: List[Webhook]
