7cf03f4657f89d70035f7b96136442b7f0ce30ae
Josh H <jhizgiaev2704@gmail.com>
2023-03-16 03:27:04 -0400

    Add options chain support

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ from dotenv import load_dotenv
 #import data science tools and API's 
 from pandas_datareader import data as pdr
 import numpy as np
+import dataframe_image as dfi
 from pytz import timezone
 import yfinance as yf
 from yahoo_fin import options as op
@@ -356,5 +357,44 @@ async def graph_indicators(ctx, left_time_point:str, right_time_point:str, stock
         embed = discord.Embed(colour=0xFF8300,title=f"Stock RSI, BBands, and VWAP for {stock} Plot")
         embed.set_image(url="attachment://output.png")
         await ctx.send(embed=embed, file=file)
+        
+@client.command(name='Options-Chain-Call',help='Outputs the call options chain for a selected stock',aliases=['opc'])
+async def get_call(ctx, stock:str, num_rows=10):
+    if(stock == None):
+        await ctx.send("ERROR: Cannot have stock=Nonetype")
+        raise discord.DiscordException("ERROR: Cannot have stock=Nonetype")
+    else:
+        expirationDates = op.get_expiration_dates(stock)
+        callData = op.get_calls(stock,date=expirationDates[0])
+        callData = callData.tail(num_rows)
+        callData = callData.reset_index(drop=True)
+        callData = callData.drop(columns=['Contract Name'])
+        dfi.export(callData,'output.png',table_conversion='matplotlib',fontsize=8)
+        file = discord.File("output.png", filename="output.png")
+        embed = discord.Embed(colour=0xFF8300,title=f"Stock call options for {stock}")
+        embed.set_image(url="attachment://output.png")
+        await ctx.send(embed=embed, file=file)
+        os.remove('output.png')
+        
+@client.command(name='Options-Chain-Put',help='Outputs the put options chain for a selected stock',aliases=['opp'])
+async def get_put(ctx, stock:str,num_rows=10):
+    if(stock == None):
+        await ctx.send("ERROR: Cannot have stock=Nonetype")
+        raise discord.DiscordException("ERROR: Cannot have stock=Nonetype")
+    elif num_rows>20:
+        await ctx.send("ERROR: Cannot exceed 20 rows!")
+        raise discord.DiscordException("ERROR: Cannot exceed 20 rows!")
+    else:
+        expirationDates = op.get_expiration_dates(stock)
+        putData = op.get_puts(stock,date=expirationDates[0])
+        putData = putData.tail(num_rows)
+        putData = putData.reset_index(drop=True)
+        putData = putData.drop(columns=['Contract Name'])
+        dfi.export(putData,'output.png',table_conversion='matplotlib',fontsize=8)
+        file = discord.File("output.png", filename="output.png")
+        embed = discord.Embed(colour=0xFF8300,title=f"Stock put options for {stock}")
+        embed.set_image(url="attachment://output.png")
+        await ctx.send(embed=embed, file=file)
+        os.remove('output.png')
 
 client.run(TOKEN)
