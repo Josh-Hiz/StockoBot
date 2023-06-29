9b66ba005a83bd2fe80e209c07b50fb3d52720e5
Josh H <jhizgiaev2704@gmail.com>
2023-06-28 23:32:02 -0400

    Add news handler

---
+++ news_handler/news.py
@@ -0,0 +1,20 @@
+# Main python file for news handler module
+# to be used in the main bot file 
+import os
+import discord
+from discord.ext import commands
+
+def parse_news(data):
+    '''
+    Parses news and grabs the following data:
+    - Title
+    - Summary
+    - URL
+    - Sentiment
+    '''
+    data["feed"] = data["feed"][:5]
+    news_emb = discord.Embed(colour=0x338AFF,title="Top 5 Articles for your stock!")
+    for i in range(len(data["feed"])):
+        news_emb.add_field(name=f'{data["feed"][i]["title"]}',value=f'{data["feed"][i]["summary"]}\n[Read More]({data["feed"][i]["url"]})\nSentiment: {data["feed"][i]["overall_sentiment_label"]}',inline=False)
+        
+    return news_emb
