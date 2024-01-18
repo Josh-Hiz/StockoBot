c0aab192cc15c47695f282553f7ef40cd6f6c4a5
Josh H <jhizgiaev2704@gmail.com>
2024-01-18 00:49:03 -0500

    Add universal query client

--- cogs/finance_cog.py
+++ cogs/finance_cog.py
@@ -1,5 +1,6 @@
 # FinanceCog.py
 from discord.ext import commands
+from query_handler.query import QueryHandler
 from statistics_handler.statistics_handler import StatisticsHandler
 from options_handler.options_handler import OptionHandler
 from news_handler.news_handler import NewsHandler
@@ -12,8 +13,9 @@ class FinanceCog(commands.Cog):
         self.stat_client = StatisticsHandler(bot)
         self.option_client = OptionHandler(bot)
         self.news_client = NewsHandler(bot)
+        self.data_query_client = QueryHandler(bot)
         # self.realtime_client = RealtimeHandler(bot)
-        
+    
     @commands.command(name='Graph-Performance',help='Returns a chart of stock performance over a time interval',aliases=['gp'])
     async def performance_graph(self, ctx, date1, date2, stock, chart_type="Line", mav_set=10):
         await self.stat_client.graph_performance(ctx, date1, date2, stock, chart_type, mav_set)
@@ -23,16 +25,20 @@ class FinanceCog(commands.Cog):
         await self.stat_client.chart_macd(ctx,date1,date2,stock)
     
     @commands.command(name="Graph-Volatility",help='Showcases historical volatility or provides volatility prediction using GARCH',aliases=['gv'])
-    async def chart_volatility(self,ctx, date1,date2,stock,option="Historical",chart_type="Line"):
-        await self.stat_client.chart_volatility(ctx,date1,date2,stock,option,chart_type)
+    async def chart_volatility(self,ctx, date1,date2,stock,chart_type="Line",trading_days=60):
+        await self.stat_client.chart_volatility(ctx,date1,date2,stock,chart_type,trading_days)
         
     @commands.command(name="Graph-Ratio",help='Showcases the specific ratio of a stock',aliases=['gr'])
-    async def chart_ratio(self,ctx, date1, date2,stock,option="Sharpe-Ratio"):
-        await self.stat_client.chart_ratio(ctx,date1,date2,stock,option)
+    async def chart_ratio(self,ctx, date1, date2,stock,option="Sharpe",trading_days=60):
+        await self.stat_client.chart_ratio(ctx,date1,date2,stock,option,trading_days)
 
     @commands.command(name='Graph-Indicators',help='Outputs a graph of 3 technical indicators, RSI, BBands, and VWAP for a selected time range and stock',aliases=['gi'])
     async def graph_indicators(self, ctx, left_time_point, right_time_point,stock):
         await self.stat_client.graph_indicators(ctx,left_time_point,right_time_point,stock)
+
+    @commands.command(name="Query-Equity",help="Outputs the overall comprehensive summary of a given valid equity",aliases=["q"])
+    async def get_summary(self,ctx,stock):
+        await self.stat_client.stock_summary(ctx, stock)
 
     @commands.command(name='Options-Chain-Call',help='Outputs the call options chain for a selected stock',aliases=['opc'])
     async def get_call(self,ctx,stock,num_rows=10):
@@ -46,9 +52,11 @@ class FinanceCog(commands.Cog):
     # async def stock_realtime(self, ctx, symbol):
     #     await self.realtime_client.stock_realtime(ctx, symbol)
 
-    @commands.command(name="news",help='Outputs financial news based on a prompt',alias='nw')
+    @commands.command(name="news",
+                      help='Outputs financial news based on a prompt\nTopics:\n- blockchain\n- earnings\n- ipo\n- mergers_and_acquisitions\n- financial_markets\n- economy_fiscal\n- economy_monetary\n- economy_macro\n- energy_transportation\n- finance\n- life_sciences\n- manufacturing\n- real_estate\n- retail_wholesale\n- technology\n',
+                      aliases=['nw'])
     async def get_news(self,ctx, stock, topics=None, num_articles=5):
         await self.news_client.get_news(ctx, stock, topics, num_articles)
-
+        
 def setup(bot):
     bot.add_cog(FinanceCog(bot))
